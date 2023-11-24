edbb0e8a7eae45a2dc075cd704d76ad8244e1117
SwyftPain <100738736+SwyftPain@users.noreply.github.com>
2023-11-24 18:24:33 +0100

    Update WPFInTS.ts

--- WPFInTS.ts
+++ WPFInTS.ts
@@ -2,25 +2,67 @@ import { Func, func } from "edge-js";
 import path from "path";
 
 interface IWindow {
+  /**
+   * Title of the main window
+   */
   title?: string;
+  /**
+   * Width of the main window
+   */
   width?: number;
+  /**
+   * Height of the main window
+   */
   height?: number;
+  /**
+   * Background color of the main window
+   */
   backgroundColor?: string;
+  /**
+   * Foreground color of the main window
+   */
   foregroundColor?: string;
 }
 
 interface ITextBlock {
+  /**
+   * Text to display in the TextBlock
+   */
   text: string;
+  /**
+   * Vertical position of the TextBlock
+   */
   verticalAlignment?: "top" | "bottom" | "center" | "stretch";
+  /**
+   * Horizontal position of the TextBlock
+   */
   horizontalAlignment?: "left" | "right" | "center" | "stretch";
 }
 
 interface IButton {
+  /**
+   * The text displayed on the button
+   */
   text: string;
+  /**
+   * @ignore The function to execute when the button is pressed
+   */
   onClick?: () => void;
+  /**
+   * Vertical position of the button
+   */
   verticalAlignment?: "top" | "bottom" | "center" | "stretch";
+  /**
+   * Horizontal position of the button
+   */
   horizontalAlignment?: "left" | "right" | "center" | "stretch";
+  /**
+   * Background color of the button
+   */
   backgroundColor?: string;
+  /**
+   * Foreground color of the button
+   */
   foregroundColor?: string;
 }
 
@@ -38,6 +80,10 @@ class MyWpfApp {
     this.wpfApp = func(path.join(__dirname, "myClass.cs"));
   }
 
+  /**
+   * Set the main window
+   * @param window Main window properties
+   */
   setWindow(window: IWindow) {
     this.title = window.title ? window.title : "WPF from Node.js";
     this.width = window.width ? window.width : 300;
@@ -50,6 +96,10 @@ class MyWpfApp {
       : "#000000";
   }
 
+  /**
+   * Add TextBlocks to the main window
+   * @param textBlock TextBlock properties
+   */
   addTextBlock(textBlock: ITextBlock) {
     const wpfTextBlock: ITextBlock = {
       text: textBlock.text,
@@ -60,6 +110,10 @@ class MyWpfApp {
     this.textBlocks.push(wpfTextBlock);
   }
 
+  /**
+   * Add Buttons to the main window
+   * @param button Button properties
+   */
   addButton(button: IButton) {
     const defaultBackgroundColor = "#FFFFFF"; // Default background color (white)
     const defaultForegroundColor = "#000000"; // Default foreground color (black)
@@ -76,6 +130,9 @@ class MyWpfApp {
     this.buttons.push(wpfButton);
   }
 
+  /**
+   * Display the GUI app
+   */
   render() {
     this.wpfApp(this, (error, result) => {
       if (error) throw error;
